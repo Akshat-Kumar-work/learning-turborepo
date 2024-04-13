38e1271a5df6290cd430b5b7d3ee93a9ebbe9af7
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-13 22:16:24 +0530

    express server setup with some ts configuration for typescript

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 
 
-app.get("/",()=>{
+app.listen(4000);
+
+app.get("/",(req,res)=>{
     console.log("hi from express server")
+    return res.json({
+        msg:"all ok"
+    })
 })
+
