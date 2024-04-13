b28f457f7a09e395a8ac68dc880977e0a0fd51cd
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-13 22:41:46 +0530

    added dependencies

--- apps/web/app/admin/page.tsx
+++ apps/web/app/admin/page.tsx
@@ -1,11 +1,12 @@
 import Admin from "@repo/ui/admin"
+import { InputBox } from "@repo/ui/input-box"
 
 const page = () => {
   return (
     <div>
 
         <Admin></Admin>
-
+        <InputBox></InputBox>
     </div>
   )
 }
